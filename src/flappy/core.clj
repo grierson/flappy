a3ea17cb8b4f5c3a41475eae14ebce39e6727f73
kyleg <kyle.grierson@nicholsplc.co.uk>
2019-06-20 11:06:22 +0100

    Update and draw

--- src/flappy/core.clj
+++ src/flappy/core.clj
@@ -1,25 +1,35 @@
 (ns flappy.core
   (:require [quil.core :as q]
+            [quil.middleware :as m]
             [clojure.java.io :as io]))
 
 (defn setup []
-  (q/frame-rate 1)                    ;; Set framerate to 1 FPS
-  (q/background (q/load-image (io/resource "PNG/background.png"))))                ;; Set the background colour to
+  (q/frame-rate 1)                                          ;; Set framerate to 1 FPS
+  (let [image (q/load-image "/src/flappy/data/PNG/background.png")] ;; Set the background colour to
+    (Thread/sleep 1000)
+    (q/background-image image))
+  {:plane (q/load-image "/src/flappy/data/PNG/Planes/planeBlue1.png")
+   :x     0
+   :y     0})
+
+(defn update-state [state]
+  (let [key (q/raw-key)]
+    (prn key)
+    (condp = key
+      \u (update state :y inc)
+      \d (update state :y dec)
+      state)))
+
 
 ;; a nice shade of grey.
-(defn draw []
-  (q/stroke (q/random 255) (q/random 255) (q/random 255))             ;; Set the stroke colour to a random grey
-  (q/stroke-weight (q/random 255))
-  (q/fill (q/random 255) (q/random 255) (q/random 255))               ;; Set the fill colour to a random grey
-
-  (let [diam (q/random 10)             ;; Set the diameter to a value between 0 and 100
-        x    (q/random (q/width))       ;; Set the x coord randomly within the sketch
-        y    (q/random (q/height))]     ;; Set the y coord randomly within the sketch
-    (q/ellipse x y diam diam)))         ;; Draw a circle at x y with the correct diameter
+(defn draw [state]
+  (q/image (:plane state) 100 (:y state)))
 
 (q/defsketch example                  ;; Define a new sketch named example
              :title "Oh so many grey circles"    ;; Set the title of the sketch
              :settings #(q/smooth 2)             ;; Turn on anti-aliasing
-             :setup setup                        ;; Specify the setup fn
-             :draw draw                          ;; Specify the draw fn
-             :size [800 480])                     ;; You struggle to beat the golden ratio
+             :setup setup                                   ;; Specify the setup fn
+             :update update-state
+             :draw draw                                     ;; Specify the draw fn
+             :size [800 480]
+             :middleware [m/fun-mode])                      ;; You struggle to beat the golden ratio
