4a8f51795af3426fc3c2dae165adf31679e82c97
Kyle Grierson <grierson@openmailbox.org>
2019-11-26 17:10:33 +0000

    init

--- src/flappy/core.clj
+++ src/flappy/core.clj
@@ -4,32 +4,54 @@
             [clojure.java.io :as io]))
 
 (defn setup []
-  (q/frame-rate 1)                                          ;; Set framerate to 1 FPS
-  (let [image (q/load-image "/src/flappy/data/PNG/background.png")] ;; Set the background colour to
-    (Thread/sleep 1000)
-    (q/background-image image))
-  {:plane (q/load-image "/src/flappy/data/PNG/Planes/planeBlue1.png")
-   :x     0
-   :y     0})
+  (let [background "./resources/PNG/background.png"
+        plane "./resources/PNG/Planes/planeGreen1.png"
+        ground "./resources/PNG/groundDirt.png"
+        ceiling "./resources/PNG/ceilingDirt.png"
+        rock "./resources/PNG/rock.png"
+        rockDown "./resources/PNG/rockDown.png"]
+    (q/set-state!
+      :image (q/load-image background)
+      :rock (q/load-image rock)
+      :rockDown (q/load-image rockDown)
+      :ground (q/load-image ground)
+      :ceiling (q/load-image ceiling)
+      :plane {:x 0
+              :y 240
+              :image (q/load-image plane)})))
 
 (defn update-state [state]
-  (let [key (q/raw-key)]
-    (prn key)
-    (condp = key
-      \u (update state :y inc)
-      \d (update state :y dec)
-      state)))
+  (if (q/key-pressed?)
+    (let [key (q/raw-key)]
+      (case key
+        \u (update-in state [:plane :y] dec)
+        \d (update-in state [:plane :y] inc)
+        state))
+    state))
 
-
-;; a nice shade of grey.
 (defn draw [state]
-  (q/image (:plane state) 100 (:y state)))
+  (q/background 255)
+  (let [img (q/state :image)
+        plane (q/state :plane)
+        ground (q/state :ground)
+        ceiling (q/state :ceiling)
+        rock (q/state :rock)
+        rockDown (q/state :rockDown)]
+    (when (q/loaded? img)
+      (q/image img 0 0))
+    (when (q/loaded? (:image plane))
+      (q/image (:image plane) (:x plane) (:y plane)))
+    (when (and (q/loaded? rock) (q/loaded? rockDown))
+      (q/image rock 600 (- 480 239))
+      (q/image rockDown 300 0))
+    (when (and (q/loaded? ground) (q/loaded? ceiling))
+      (q/image ground 0 409)
+      (q/image ceiling 0 0))))
 
-(q/defsketch example                  ;; Define a new sketch named example
-             :title "Oh so many grey circles"    ;; Set the title of the sketch
-             :settings #(q/smooth 2)             ;; Turn on anti-aliasing
-             :setup setup                                   ;; Specify the setup fn
+(q/defsketch example
+             :title "Flappy"
+             :setup setup
              :update update-state
-             :draw draw                                     ;; Specify the draw fn
+             :draw draw
              :size [800 480]
-             :middleware [m/fun-mode])                      ;; You struggle to beat the golden ratio
+             :middleware [m/fun-mode])
